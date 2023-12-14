f765871751f3bcfe6e937e687cfd6adf75d7476c
Litalhag <litalhag@gmail.com>
2023-12-14 15:38:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Container } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        <h1>My App</h1>
+      </Container>
+    </Box>
+  )
+}
+
+export default App
